73d74b99b576017d0d91052cdceb3729cada283e
Ezer Isai Angeles <ezerangeles1@gmail.com>
2021-06-09 14:08:48 -0700

    Feature: Added Route guarding
    Included a new file, authUserRoute.js, that returns the respective component if user is logged in. Updated loginForm.js to handle user login and auth states.

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Main, Login, OrderForm, ViewOrders } from '../components';
+import AuthUserRoute from './authUserRoute';
 
 const AppRouter = (props) => {
   return (
     <Router>
       <Route path="/" exact component={Main} />
       <Route path="/login" exact component={Login} />
-      <Route path="/order" exact component={OrderForm} />
-      <Route path="/view-orders" exact component={ViewOrders} />
+      <AuthUserRoute path="/order" exact component={OrderForm} />
+      <AuthUserRoute path="/view-orders" exact component={ViewOrders} />
     </Router>
   );
 }
